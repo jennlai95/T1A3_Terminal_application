9da14c0da2975f5f9fb242a554033f9b8697e295
jennlai95 <jenniferlaai@gmail.com>
2023-05-01 18:04:57 +1000

    created menu and welcome message

---
+++ src/main.py
@@ -0,0 +1,8 @@
+print ("Welcome to our Hotel booking!")
+
+def create_menu():
+    print("Enter 1 to view user information")
+    print("Enter 2 to view available rooms")
+    print("Enter 3 to view previous booking")
+    print("Enter 4 to view receipt")
+    print("Enter 5 to exit")
