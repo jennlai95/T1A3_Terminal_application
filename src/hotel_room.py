d81e29003d1d8e9e85cba24df74a3f850f4acf11
jennlai95 <jenniferlaai@gmail.com>
2023-05-02 23:40:26 +1000

    added hotel_room.py

---
+++ src/hotel_room.py
@@ -0,0 +1,11 @@
+class Room:
+    def _init_(self,room_type,price,capacity):
+        self.type = room_type
+        self.price = price
+        self.capacity = capacity
+        print(self)
+    
+ #room_type lust  
+room_type = [('single'), ('double'), ('twin'),('queen')]
+    
+ 
--- src/main.py
+++ src/main.py
@@ -1,8 +1,37 @@
+from hotel_room import Room
+from hotel_room import room_type
+
 print ("Welcome to our Hotel booking!")
 
 def create_menu():
     print("Enter 1 to view user information")
     print("Enter 2 to view available rooms")
-    print("Enter 3 to view previous booking")
+    print("Enter 3 to view current booking")
     print("Enter 4 to view receipt")
     print("Enter 5 to exit")
+    choice = input("Enter your selection:")
+    return choice
+
+user_choice = ""
+print(user_choice)
+
+while user_choice != "5":
+    user_choice = create_menu()
+    
+    if (user_choice == "1"):
+        print("Add user_info")
+    elif (user_choice == "2"):
+        print(room_type)
+    elif (user_choice == "3"):
+        print("current booking")
+    elif (user_choice == "4"):
+        print("receipt")
+    elif (user_choice == "5"):
+        continue
+    else: 
+        print("Invalid input")
+    
+    input("press enter to continue....")
+
+
+print("Thank you for using the booking app")
